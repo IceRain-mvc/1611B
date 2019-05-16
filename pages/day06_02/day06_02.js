75fcb8112624e77b6fa2762bdba623000c41869f
cuilibao <cuilibao123@gmail.com>
2019-05-16 11:36:25 +0800

    '验证码'

--- pages/day06_02/day06_02.js
+++ pages/day06_02/day06_02.js
@@ -1,75 +1,85 @@
-// pages/day06/day06.js
+// pages/day06_02/day06_02.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    index:-1,
-    array: ["帅锅", "美女", "未知"], 
-    time:"08:30"
-  },
-
-  pickerChanger(event){
-    console.log(event)
-    this.setData({
-      index: event.detail.value
-    })
-  },
-
-  pickerTime(event){
-    this.setData({
-      time:event.detail.value
-    })
-
+    content:[],
+    limit:20
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log("onLoad")
+    let content = [];
+    wx.request({
+      url: 'https://www.xiachufang.com/juno/weapp/v2/search/universal_search.json?q=土豆',
+      data:{},
+      success:(res)=>{
+        this.setData({
+          content: res.data.content.content
+        })
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    console.log("onReady")
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    console.log("onShow")
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    console.log("onHide")
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    console.log("onUnload")
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
+    console.log("我是有底线的")
+    //加载更多数据
+    wx.request({
+      url: 'https://www.xiachufang.com/juno/weapp/v2/search/universal_search.json?q=土豆',
+      data: {
+        limit: this.data.limit+5
+      },
+      success: (res) => {
+        this.setData({
+          content: res.data.content.content
+        })
+        this.setData({
+          limit:this.data.limit+5
+        })
+      }
+    })
 
   },
 
@@ -77,6 +87,6 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    console.log("转发")
   }
 })
--- pages/day07/day07.js
+++ pages/day07/day07.js
@@ -1,62 +1,77 @@
-// pages/day06/day06.js
+// pages/day07/day07.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    index:-1,
-    array: ["帅锅", "美女", "未知"], 
-    time:"08:30"
-  },
-
-  pickerChanger(event){
-    console.log(event)
-    this.setData({
-      index: event.detail.value
-    })
-  },
-
-  pickerTime(event){
-    this.setData({
-      time:event.detail.value
-    })
-
+    obj:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log("onLoad")
+    wx.request({
+      url: 'https://auth_services.teambition.com/captcha/setup?num=5&lang=zh',
+      success:(res)=>{
+        this.setData({
+          obj:res.data
+        })
+      }
+
+    })
+  },
+
+  clickImg(event){ // change
+    let {index} = event.target.dataset;
+    wx.request({
+      url: 'https://auth_services.teambition.com/captcha/valid?num=5&uid='+this.data.obj.uid+'&value='+this.data.obj.values[index]+'&lang=zh',
+      success:(res)=>{
+        console.log(res.data);
+        if (res.data.valid){
+          wx.showModal({
+            title: '验证结果',
+            content: '正确',
+          })
+        }else{
+          wx.showModal({
+            title: '验证结果',
+            content: '错误',
+          })
+        }
+
+      }
+
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    console.log("onReady")
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    console.log("onShow")
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    console.log("onHide")
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    console.log("onUnload")
+
   },
 
   /**
